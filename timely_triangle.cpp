7be63b1eee7edc97029b6cb5f086cc0a1df74b04
sidd <n2verz@gmail.com>
2024-08-13 13:56:20 -0400

    Linked Shader objects to Shader Program!

--- timely_triangle.cpp
+++ timely_triangle.cpp
@@ -60,6 +60,67 @@ int main()
     */ 
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+    // Our Vertex Shader we made. Bigger shaders probably will be linked through different files rather than doing making it here.
+    const char *vertexShaderSource = "#version 330 core\n" 
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main() {\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}/0";
+
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL); // The 1 signifies how many strings we're passing as a source code.
+    glCompileShader(vertexShader); // Compile Shader
+
+    // Check to see if compilation is successfull!
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success); // command to check compilation
+    if (!success) 
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog); // Get info from failed compilation
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    // Fragment Shader
+    const char *fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main() {\n"
+    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\0";
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success) 
+    {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog); // Get info from failed compilation
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+
+    // Create Shader Program by linking our shaders in!
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    // Check Shader Program (different from shader checks)
+    glGetProgramiv(shaderProgram, GL_COMPILE_STATUS, &success);
+    if (!success) 
+    {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    // No longer need shader objects as they've been linked
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    glUseProgram(shaderProgram);
+
     // Simple Render Loop
     while (!glfwWindowShouldClose(window)) 
     {
