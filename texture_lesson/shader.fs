6ccb2742357a0565937f382f7901cee9b1046f04
sidd <n2verz@gmail.com>
2025-01-20 10:02:44 -0500

    disco texture!

--- texture_lesson/shader.fs
+++ texture_lesson/shader.fs
@@ -8,5 +8,5 @@ uniform sampler2D ourTexture; //the texture reference
 
 void main()
 {
-    FragColor = texture(ourTexture, texCoord);
+    FragColor = texture(ourTexture, texCoord) * vec4(color, 1.0);
 }
