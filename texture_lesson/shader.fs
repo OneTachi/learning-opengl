0b37e561fc19796421700581f9bc301110cc8c97
sidd <n2verz@gmail.com>
2025-01-19 23:39:23 -0500

    alter fragment shader for texture

--- texture_lesson/shader.fs
+++ texture_lesson/shader.fs
@@ -1,9 +1,12 @@
-# version 330
+#version 330 core
 
 in vec3 color;
+in vec2 texCoord;
+
 out vec4 FragColor;
+uniform sampler2D ourTexture; #the texture reference
 
 void main()
 {
-    FragColor = vec4(color, 1.0);
+    FragColor = texture(ourTexture, texCoord);
 }
